7412d1a54a4d2c1e66bb131255df5c83d1be2a2a
Frederik K <frederik.regi@gmail.com>
2022-10-10 18:20:34 +0200

    Introduced array group structs  for all flow variables

---
+++ src/FlowVariableGroupStructs.h
@@ -0,0 +1,76 @@
+/*
+ * FlowVariableScalarStructs.h
+ *
+ *  Created on: Oct 10, 2022
+ *      Author: frederk
+ */
+
+#ifndef SRC_FLOWVARIABLEGROUPSTRUCTS_H_
+#define SRC_FLOWVARIABLEGROUPSTRUCTS_H_
+
+
+struct ConservedVariablesScalars
+{
+public:
+	double rho;		// Mass density
+	double rho_u;	// Momentum density in x-direction
+	double rho_v;	// Momentum density in y-direction
+	double rho_w;	// Momentum density in z-direction
+	double E;		// Total specific energy per volume
+
+	ConservedVariablesScalars(double rho, double rho_u, double rho_v, double rho_w, double E) :
+		rho{rho}, rho_u{rho_u}, rho_v{rho_v}, rho_w{rho_w}, E{E}
+	{}
+};
+
+struct ConservedVariablesArrayGroup
+{
+	Array3D_d rho;		// Mass density
+	Array3D_d rho_u;	// Momentum density in x-direction
+	Array3D_d rho_v;	// Momentum density in y-direction
+	Array3D_d rho_w;	// Momentum density in z-direction
+	Array3D_d rho_E;	// Total specific energy per volume
+
+	ConservedVariablesArrayGroup(uint gridSizeX, uint gridSizeY, uint gridSizeZ) :
+	rho(gridSizeX, gridSizeY, gridSizeZ),
+	rho_u(gridSizeX, gridSizeY, gridSizeZ),
+	rho_v(gridSizeX, gridSizeY, gridSizeZ),
+	rho_w(gridSizeX, gridSizeY, gridSizeZ),
+	rho_E(gridSizeX, gridSizeY, gridSizeZ)
+	{}
+};
+
+struct PrimitiveVariablesArrayGroup
+{
+	Array3D_d u;	// Velocity component in x-direction
+	Array3D_d v;	// Velocity component in y-direction
+	Array3D_d w;	// Velocity component in z-direction
+	Array3D_d p;	// Pressure
+	Array3D_d T;	// Temperature
+
+	PrimitiveVariablesArrayGroup(uint gridSizeX, uint gridSizeY, uint gridSizeZ) :
+	u(gridSizeX, gridSizeY, gridSizeZ),
+	v(gridSizeX, gridSizeY, gridSizeZ),
+	w(gridSizeX, gridSizeY, gridSizeZ),
+	p(gridSizeX, gridSizeY, gridSizeZ),
+	T(gridSizeX, gridSizeY, gridSizeZ)
+	{}
+};
+
+struct TransportPropertiesArrayGroup
+{
+	Array3D_d mu;		// Dynamic viscosity
+	Array3D_d kappa;	// Thermal conductivity
+
+	TransportPropertiesArrayGroup(uint gridSizeX, uint gridSizeY, uint gridSizeZ) :
+	mu(gridSizeX, gridSizeY, gridSizeZ),
+	kappa(gridSizeX, gridSizeY, gridSizeZ)
+	{}
+};
+
+
+#endif /* SRC_FLOWVARIABLEGROUPSTRUCTS_H_ */
+
+
+
+
--- src/Mesh.h
+++ src/Mesh.h
@@ -10,7 +10,7 @@
 
 #include "includes_and_names.h"
 #include "Array3D_d.h"
-#include "FlowVariableScalarStructs.h"
+#include "FlowVariableGroupStructs.h"
 
 class Mesh
 {
@@ -23,16 +23,11 @@ public:
 	void swapConservedVariables();
 	uint NI, NJ, NK;									// Mesh size. Number of nodes in x,y,z directions
 	uint nNodesTotal;									// Total number of nodes in the mesh
-	Array3D_d rho, rho_u, rho_v, rho_w, E;				// Conserved variables
-	Array3D_d u, v, w, p, T;							// Primitive variables
-	Array3D_d mu, kappa;								// Transport properties
-	Array3D_d k1_rho  , k2_rho  , k3_rho  , k4_rho  ;	// RK4 stages, continuity
-	Array3D_d k1_rho_u, k2_rho_u, k3_rho_u, k4_rho_u;	// RK4 stages, x-momentum
-	Array3D_d k1_rho_v, k2_rho_v, k3_rho_v, k4_rho_v;	// RK4 stages, y-momentum
-	Array3D_d k1_rho_w, k2_rho_w, k3_rho_w, k4_rho_w;	// RK4 stages, z-momentum
-	Array3D_d k1_E    , k2_E    , k3_E    , k4_E    ;	// RK4 stages, specific total energy
-	Array3D_d interm_rho, interm_rho_u, interm_rho_v,	// Intermediate solutions.
-	interm_rho_w, interm_E;
+	ConservedVariablesArrayGroup conservedVariables;	// Mass density, momentum & total energy
+	PrimitiveVariablesArrayGroup primitiveVariables;	// Velocity, pressure & Temperature
+	TransportPropertiesArrayGroup transportProperties;	// Viscosity and thermal conductivity
+	ConservedVariablesArrayGroup RK4_slopes[4];			// 4 arrays for each conserved variable
+	ConservedVariablesArrayGroup conservedVariablesNew;	// Intermediate states of conserved variables.
 	double dx, dy, dz;									// Grid spacing in x-, y- and z-direction
 private:
 	void setGridSpacings(double domainLengthX, double domainLengthY, double domainLengthZ );
